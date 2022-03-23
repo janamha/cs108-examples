76a85f400d77fd7e226da062cd21ee7308c30d24
Jana Aguilar <janamha@bu.edu>
2022-03-23 13:35:23 -0400

    Assignment 15 update of templates

--- pages/urls.py
+++ pages/urls.py
@@ -7,6 +7,5 @@ from .views import BasePageView, HomePageView, AboutPageView, SchedulePageView
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about', AboutPageView.as_view(), name='about'),
-    path('base', BasePageView.as_view(), name='base'),
     path('schedule', SchedulePageView.as_view(), name='schedule'),
-]
+] 
