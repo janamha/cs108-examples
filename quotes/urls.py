0209aefdc6f9beb8bf119bafb4ebf51623b6ea4a
Jana Aguilar <janamha@bu.edu>
2022-03-28 02:25:38 -0400

    Refined model design to create Person, Quote, and Image models.

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,9 +1,12 @@
 ##### quotes/urls.py #####
 
 from django.urls import path
-from .views import HomePageView # our view class definition 
+from .views import HomePageView, QuotePageView, RandomQuoteView, PersonPageView # our view class definition 
 
 urlpatterns = [
     # map the URL (empty string) to the view
-    path('', HomePageView.as_view(), name='home'), # generic class-based view
+    path('all', HomePageView.as_view(), name='all'), # generic class-based view
+    path('quote/<int:pk>', QuotePageView.as_view(), name='quote'), # show one quote
+    path('', RandomQuoteView.as_view(), name='random'),  # show one quote at random
+    path('person/<int:pk>', PersonPageView.as_view(), name="person"), ## NEW URL TO SHOW PERSON PAGE
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,8 @@
 ##### quotes/views.py #####
 
-from .models import Quote
-from django.views.generic import ListView
+from .models import Quote, Person
+from django.views.generic import ListView, DetailView
+import random
 
 class HomePageView(ListView):
     '''Create a subclass of ListView to display all quotes.'''
@@ -9,3 +10,30 @@ class HomePageView(ListView):
     model = Quote # retrieve objects of type Quote from the database
     template_name = 'quotes/home.html'
     context_object_name = 'quotes' # how to find the data in the template file
+
+class QuotePageView(DetailView):
+    '''Show the details for one quote.'''
+    model = Quote
+    template_name = 'quotes/quote.html'
+    context_object_name = 'quote'
+
+class RandomQuoteView(DetailView):
+    '''Show the details for one quote.'''
+    model = Quote
+    template_name = 'quotes/quote.html'
+    ## note: reusing same template as DetailView to show one quote!!
+    context_object_name = 'quote'
+
+    # pick one quote at random:
+    def get_object(self):
+        '''Return one Quote object chosen at random.'''
+		
+        all_quotes = Quote.objects.all()
+        return random.choice(all_quotes)
+
+class PersonPageView(DetailView):
+    '''Show all quotes and all images for one person.'''
+
+    model = Person
+    template_name = 'quotes/person.html'
+    context_object_name = 'person'
