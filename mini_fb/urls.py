978b472fcdb07a44beb3e1909aa7c0542f8a21a6
Jana Aguilar <janamha@bu.edu>
2022-03-28 13:48:53 -0400

    Created show profile page feature

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,9 +1,10 @@
 ##### mini_fb/urls.py #####
 
 from django.urls import path
-from .views import ShowAllProfilesView # our view class definition 
+from .views import ShowAllProfilesView, ShowProfilePageView # our view class definition 
 
 urlpatterns = [
     # map the URL (empty string) to the view
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'), # generic class-based view
+    path('profile/<int:pk>', ShowProfilePageView.as_view(), name='show_profile_page'), # show one quote
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -4,7 +4,7 @@
 # Description: Creates views to display profiles
 
 from .models import Profile
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class ShowAllProfilesView(ListView):
     '''Create a subclass of ListView to display all profiles.'''
@@ -12,3 +12,10 @@ class ShowAllProfilesView(ListView):
     model = Profile # retrieve objects of type Profile from the database
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles' # how to find the data in the template file
+
+
+class ShowProfilePageView(DetailView):
+    '''Create a subclass of DetailView to display the info of one profile.'''
+    model = Profile # retrieve objects of type Profile from the database
+    template_name = 'mini_fb/show_profile_page.html'
+    context_object_name = 'profile' # how to find the data in the template file
