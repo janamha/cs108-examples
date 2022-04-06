2b28714f7384c5fb043922fa969e8cee968530ee
Jana Aguilar <janamha@bu.edu>
2022-04-06 16:13:23 -0400

    Edited comments

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -10,7 +10,7 @@ from .views import DeleteStatusMessageView, ShowAllProfilesView, ShowProfilePage
 urlpatterns = [
     # map the URL (empty string) to the view
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'), # generic class-based view
-    path('profile/<int:pk>', ShowProfilePageView.as_view(), name='show_profile_page'), # show one quote
+    path('profile/<int:pk>', ShowProfilePageView.as_view(), name='show_profile_page'), # show one profile
     path('create_profile', CreateProfileView.as_view(), name='create_profile'), ## NEW
     path('profile/<int:pk>/update', UpdateProfileView.as_view(), name="update_profile"), 
     path('profile/<int:pk>/post_status', post_status_message, name="post_status"), 
