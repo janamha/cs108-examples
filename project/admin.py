20d54a3301a7e8f4334a23c6b4fea4d192709530
Jana Aguilar <janamha@bu.edu>
2022-05-04 13:21:20 -0400

    Fixed suggestion model

--- project/admin.py
+++ project/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 
 # Register your models here.
-from .models import Dancer, Choreographer, Room, Dance
+from .models import Dancer, Choreographer, Room, Dance, Suggestion
 
 admin.site.register(Dancer)
 admin.site.register(Choreographer)
 admin.site.register(Room)
 admin.site.register(Dance)
+admin.site.register(Suggestion)
